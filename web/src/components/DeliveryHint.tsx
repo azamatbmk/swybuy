a90c6213775fd53fab1486305eee9d6a10fbb5c1
Azamat <azamat.kudziev@gmail.com>
2026-08-26 19:40:48 +0300

    Исправлили файл сервера

--- web/src/components/DeliveryHint.tsx
+++ web/src/components/DeliveryHint.tsx
@@ -5,9 +5,3 @@ export function DeliveryHint() {
     </div>
   );
 }
-  return (
-    <div className="rounded-3xl bg-lavender/50 px-5 py-4 text-sm">
-      По Северной Осетии доставка в день покупки бесплатно
-    </div>
-  );
-}
